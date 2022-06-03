7328138e3188b50410343621af992ee69233af26
Will Conrad (PC) <jwconrad23@gmail.com>
2022-06-02 22:51:05 -0400

    split into classes

--- src/main/java/Network.java
+++ src/main/java/Network.java
@@ -3,31 +3,41 @@ import org.ejml.simple.SimpleMatrix;
 import basicneuralnetwork.NeuralNetwork;
 import basicneuralnetwork.activationfunctions.ActivationFunction;
 
+
+
 public class Network {
+  double[][] inputs, targets; 
+  NeuralNetwork nn;
+  Dataset d;
+  private void init()
+  {
+    d = new Dataset("src/main/resources/data.csv");
 
-  // Example program to test the basic neural network by letting it solve XOR
-  public static void main(String args[]){
+    inputs = d.getInputs();
 
-      Dataset d = new Dataset("src/main/resources/data.csv");
+    targets = d.getOutputs();
 
-      double[][] trainingDataInputs = d.getInputs();
-
-      double[][] trainingDataTargets = d.getOutputs();
-      //NeuralNetwork nn = NeuralNetwork.readFromFile("src/main/resources/rgbModel.json");
-      NeuralNetwork nn = new NeuralNetwork(3, 2, 9, 10);
-      nn.setActivationFunction(ActivationFunction.SIGMOID);
-      nn.setLearningRate(0.01);
-      for (int i = 0; i < 5000000; i++) {
-          // training in random order
-          int random = (int)(Math.random() * d.getLength());
-          nn.train(trainingDataInputs[random], trainingDataTargets[random]);
-          System.out.println((float)(i) / 5000000 * 100 + "%");
-          //if (i % 1000000 == 0) nn.mutate(0.1);
-      }
-      double correct = 0;
+    nn = new NeuralNetwork(3, 2, 9, 10);
+    nn.setActivationFunction(ActivationFunction.SIGMOID);
+    nn.setLearningRate(0.01);
+  }
+  private void train(int iters)
+  {
+    for (int i = 0; i < iters; i++) {
+      // training in random order
+      int random = (int)(Math.random() * d.getLength());
+      nn.train(inputs[random], targets[random]);
+      System.out.println((float)(i) / iters * 100 + "%");
+      //if (i % 1000000 == 0) nn.mutate(0.1);
+    }
+    nn.writeToFile();
+  }
+  private double getAccuracy()
+  {
+    double correct = 0;
       for (int i = 0; i < d.getLength(); i++) 
       {
-        double[] guess = nn.guess(trainingDataInputs[i]);
+        double[] guess = nn.guess(inputs[i]);
         int index = 0;
         double value = guess[0];
         for (int e = 0; e < 10; e++)
@@ -38,7 +48,7 @@ public class Network {
             value = guess[e];
           }
         }
-        double[] answer = trainingDataTargets[i];
+        double[] answer = targets[i];
         int correctIndex = 0;
         for (int h = 0; h < 10; h++)
         {
@@ -46,9 +56,15 @@ public class Network {
         }
         if (index == correctIndex) { correct++; }
       }
-      System.out.println("accuracy: " + correct / d.getLength());
-
-      nn.writeToFile("src/main/resources/rgbModel");
+      return correct / d.getLength();
+  }
+  // Example program to test the basic neural network by letting it solve XOR
+  public static void main(String args[]){
+      Network network = new Network();
+      network.init();
+      network.train(10);
+      System.out.println(network.getAccuracy());
+      
   }
 
 
